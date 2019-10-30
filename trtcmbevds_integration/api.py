34bf45791f8f8fbd9a70d6b6c47e353a0758a8cf
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-10-31 01:09:24 +0300

    Project initiation

---
+++ trtcmbevds_integration/api.py
@@ -0,0 +1,22 @@
+import frappe
+import urllib.request
+import json
+
+
+@frappe.whitelist()
+def test_evdsintegration(testtoken):
+    url = frappe.db.get_single_value("TR TCMB EVDS Integration Settings", "server")
+    servicepath = "/service/evds/"
+    servicetype = "serieList/"
+    # her sorgunun ayrı başlığı var
+    servicedata = ""
+    servicedata = servicedata + "&key=" + testtoken
+    servicedata = servicedata + "&type=" + frappe.db.get_single_value("TR TCMB EVDS Integration Settings", "type")
+    servicedata = servicedata + "&code=" + "TP.DK.USD.A"
+
+    requesturl = url + servicepath + servicetype + servicedata
+
+    req = urllib.request.Request(requesturl)
+    response = urllib.request.urlopen(req)
+    with response as f:
+        return f.read().decode('utf-8')
---
+++ trtcmbevds_integration/trtcmbevds_integration/doctype/tr_tcmb_evds_company_settings/tr_tcmb_evds_company_settings.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2019, Framras AS-Izmir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TR TCMB EVDS Company Settings', {
+	// refresh: function(frm) {
+
+	// }
+		test_integration: function(frm){
+	    if(frm.doc.key!=""){
+	        frappe.call({
+	            method: "trtcmbevds_integration.api.test_evdsintegration",
+	            args:{
+	                testtoken: frm.doc.key
+	            },
+	            callback: function(r){
+                    frm.set_value("integrationresult", r.message)
+	            }
+	        })
+	    }
+	}
+});
